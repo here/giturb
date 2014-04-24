50a9a399a880a54e40a317c94f94c62ad67304d1
aint here <mike@herebox.org>
2014-04-24 00:50:55 -0700

    docs path and sh

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 
 
+# https://docs.python.org/2/install/index.html#modifying-python-s-search-path
+# fix import module path error for pyvenv-3.3 from cron 
+# site.py in standard lib
+# import sys
+# sys.path.append('/www/python/')
+
+
 # http://docs.python-requests.org/en/latest/user/quickstart/#make-a-request
+# fails when run from cron due to py2.7 on sys and this py3.3 in venv
 import requests
 
 # http://amoffat.github.io/sh/
 from sh import git, curl
 
-# Python Git Libraries
+# Other Python Git Libraries
 # Gittle -- soft fail requires --allow-external mimer
 # https://github.com/FriendCode/gittle
 # Pygit2 -- fail could not install with py3
@@ -15,14 +23,19 @@ from sh import git, curl
 # https://github.com/gitpython-developers/GitPython
 # from git import *
 
+# dir, file, repopath
 d = '/home/known/a/code/py/pyreqgit/weburbanist.com/'
 f = 'index.html'
 repopath = d
 
+# git, curl
 g = git.bake(_cwd=repopath)
-c = git.bake(curl)
+c = curl
 
+# req
 req = requests.get('http://weburbanist.com')
+# using sh.curl
+# req = c.('http://weburbanist.com')
 
 out = open(d+f,'w',encoding='utf-8')
 out.write(req.text)
